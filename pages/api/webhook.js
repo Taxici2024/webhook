6d5fae2f1430290a96c8bb38fa959f5ddb6babee
Taxici2024 <vicente.dela.rosa@hotmail.com>
2025-07-01 08:30:55 -0600

    crear webhook correctamente

---
+++ pages/api/webhook.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  const VERIFY_TOKEN = "taxci2024";
+
+  if (req.method === "GET") {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode && token === VERIFY_TOKEN) {
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+}
